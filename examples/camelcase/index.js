975b067109b751c569ee71f1ded4cb444339d64f
wutubin <wutubin@cr-health.com>
2019-12-08 15:53:35 +0800

    travis.yml

---
+++ examples/camelcase/index.js
@@ -0,0 +1,4 @@
+const CamelCase = require('../../packages/camelcase')
+
+let str = CamelCase('boor.baa', 'css')
+console.log(str)
---
+++ examples/uppercamelcase/index.js
@@ -0,0 +1,4 @@
+const CamelCase = require('../../packages/uppercamelcase')
+
+let str = CamelCase('boor.baa', 'css')
+console.log(str)
